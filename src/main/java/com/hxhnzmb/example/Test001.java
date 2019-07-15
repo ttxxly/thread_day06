038c3a1183219930f880e3d1114abebb3b2c5c6a
李耀 <yao.li@sugardt.com>
2019-07-15 17:01:05 +0800

    Initial commit

---
+++ src/main/java/com/hxhnzmb/example/Test001.java
@@ -0,0 +1,21 @@
+package com.hxhnzmb.example;
+
+/**
+ * 描述:
+ * 公司信息: 星辰科技有限公司 研发部
+ * 作者QQ：1952500855
+ *
+ * @author hxhxnzmb
+ * @version v1.0
+ * @date 2019/7/15 16:58
+ */
+public class Test001 {
+
+    /**
+     * 阻塞式队列最大的好处是能够防止队列容器溢出，防止丢失数据
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
