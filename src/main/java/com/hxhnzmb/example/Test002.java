638eb49a436aa63f76eb4843ca122b6c0c843bc1
李耀 <yao.li@sugardt.com>
2019-07-17 10:22:09 +0800

    调整类的位置。

--- src/main/java/com/hxhnzmb/example/Test002.java
+++ src/main/java/com/hxhnzmb/example/Test002.java
@@ -6,6 +6,36 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * 描述: 使用 BlockingQueue 模拟生产者和消费者模型
+ * 公司信息: 星辰科技有限公司 研发部
+ * 作者QQ：1952500855
+ *
+ * @author hxhxnzmb
+ * @version v1.0
+ * @date 2019/7/15 17:24
+ */
+public class Test002 {
+
+    public static void main(String[] args) {
+        BlockingQueue<String> blockingQueue = new LinkedBlockingQueue<String>(10);
+        ProducerThread producerThread = new ProducerThread(blockingQueue);
+        ConsumerThread consumerThread = new ConsumerThread(blockingQueue);
+        Thread p1 = new Thread(producerThread);
+        p1.start();
+        Thread c1 = new Thread(consumerThread);
+        c1.start();
+
+        try {
+            Thread.sleep(1000 * 10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        producerThread.stop();
+    }
+}
+
+/**
  * 生产者 添加队列
  */
 class ProducerThread implements Runnable {
@@ -74,34 +104,5 @@ class ConsumerThread implements Runnable{
             System.out.println("###消费者线程已经停止###");
         }
 
-    }
-}
-/**
- * 描述: 使用 BlockingQueue 模拟生产者和消费者模型
- * 公司信息: 星辰科技有限公司 研发部
- * 作者QQ：1952500855
- *
- * @author hxhxnzmb
- * @version v1.0
- * @date 2019/7/15 17:24
- */
-public class Test002 {
-
-    public static void main(String[] args) {
-        BlockingQueue<String> blockingQueue = new LinkedBlockingQueue<String>(10);
-        ProducerThread producerThread = new ProducerThread(blockingQueue);
-        ConsumerThread consumerThread = new ConsumerThread(blockingQueue);
-        Thread p1 = new Thread(producerThread);
-        p1.start();
-        Thread c1 = new Thread(consumerThread);
-        c1.start();
-
-        try {
-            Thread.sleep(1000 * 10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        producerThread.stop();
     }
 }
